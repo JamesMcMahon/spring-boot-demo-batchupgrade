a1ad36ccbb6005fedf4041d3e124ce8d7d777c92
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-05-12 10:27:39 -0500

    Use camel case for class name

--- src/main/java/sh/jfm/springbootdemos/batchupgradeexample/BatchUpgradeExampleApplication.java
+++ src/main/java/sh/jfm/springbootdemos/batchupgradeexample/BatchUpgradeExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BatchupgradeexampleApplication {
+public class BatchUpgradeExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BatchupgradeexampleApplication.class, args);
+		SpringApplication.run(BatchUpgradeExampleApplication.class, args);
 	}
 
 }
