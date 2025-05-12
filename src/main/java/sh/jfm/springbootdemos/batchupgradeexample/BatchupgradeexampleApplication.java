08f5ca7f17e8aebbd82039b7bf90b380a9452684
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-05-12 10:24:08 -0500

    Spring Init

---
+++ src/main/java/sh/jfm/springbootdemos/batchupgradeexample/BatchupgradeexampleApplication.java
@@ -0,0 +1,13 @@
+package sh.jfm.springbootdemos.batchupgradeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchupgradeexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchupgradeexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sh/jfm/springbootdemos/batchupgradeexample/BatchupgradeexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.jfm.springbootdemos.batchupgradeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchupgradeexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
