8fdb4c2fd1f21147b66e1b7b5a1c23e9ed90791f
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-05-13 14:54:17 -0500

    Upgrade to Spring Boot 3

--- src/main/java/sh/jfm/springbootdemos/batchupgradeexample/BatchUpgradeExampleApplication.java
+++ src/main/java/sh/jfm/springbootdemos/batchupgradeexample/BatchUpgradeExampleApplication.java
@@ -11,8 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BatchUpgradeExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchUpgradeExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BatchUpgradeExampleApplication.class, args);
+    }
 
 }
