7c2168aa1c38b1b77007063cf6beefc664aa363b
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-05-13 14:54:17 -0500

    Upgrade to Spring Boot 3

--- src/main/java/sh/jfm/springbootdemos/batchupgradeexample/BatchConfig.java
+++ src/main/java/sh/jfm/springbootdemos/batchupgradeexample/BatchConfig.java
@@ -2,10 +2,10 @@ package sh.jfm.springbootdemos.batchupgradeexample;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -14,6 +14,7 @@ import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -24,18 +25,11 @@ import javax.sql.DataSource;
  * It defines beans for readers, writers, processors, and job execution steps.
  */
 @Configuration
-@EnableBatchProcessing
 public class BatchConfig {
-
-    public final JobBuilderFactory jobBuilderFactory;
-
-    public final StepBuilderFactory stepBuilderFactory;
 
     public final DataSource dataSource;
 
-    public BatchConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource) {
-        this.jobBuilderFactory = jobBuilderFactory;
-        this.stepBuilderFactory = stepBuilderFactory;
+    public BatchConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
@@ -49,18 +43,22 @@ public class BatchConfig {
     }
 
     @Bean
-    public Job importUserJob(JobCompleteLoggerListener listener) {
-        return jobBuilderFactory.get("importUserJob")
+    public Job importUserJob(
+            JobRepository jobRepository,
+            PlatformTransactionManager transactionManager,
+            JobCompleteLoggerListener listener
+    ) {
+        return new JobBuilder("importUserJob", jobRepository)
                 .incrementer(new RunIdIncrementer())
                 .listener(listener)
-                .flow(step1())
+                .flow(step1(jobRepository, transactionManager))
                 .end()
                 .build();
     }
 
-    private Step step1() {
-        return stepBuilderFactory.get("step1")
-                .<User, User>chunk(10)
+    private Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step1", jobRepository)
+                .<User, User>chunk(10, transactionManager)
                 .reader(reader())
                 .processor(new UserNameProcessor())
                 .writer(writer())
