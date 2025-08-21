16490a72665a93a0007cfc4d4e79591c82aa8155
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-08-21 16:06:12 -0500

    This works! needs clean up

--- src/main/java/sh/jfm/springbootdemos/batchupgradeexample/DatabaselessBatchConfig.java
+++ src/main/java/sh/jfm/springbootdemos/batchupgradeexample/DatabaselessBatchConfig.java
@@ -2,8 +2,9 @@ package sh.jfm.springbootdemos.batchupgradeexample;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.configuration.BatchConfigurationException;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.ResourcelessJobRepository;
 import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
@@ -14,60 +15,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
-/**
- * The following beans configure a minimal batch processing environment without a database:
- * <ul>
- * <li>ResourcelessTransactionManager: Provides transaction management without a real transaction store</li>
- * <li>ResourcelessJobRepository: Provides job execution context without persistent storage</li>
- * <li>TaskExecutorJobLauncher: Handles job execution without depending on database resources</li>
- * </ul>
- * <p>
- * This configuration is suitable for simple batch operations that don't require
- * persistent job repository storage or transaction management.
- * <p>
- * This configuration also excludes DataSourceAutoConfiguration. This is necessary because otherwise the application will
- * attempt to autoconfigure a duplicate JobRepository bean.
- *
- * @see <a href="https://stackoverflow.com/a/79492398">Stack Overflow</a>
- * @see org.springframework.batch.core.repository.JobRepository
- */
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+@SuppressWarnings("NullableProblems")
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-public class DatabaselessBatchConfig {
+public class DatabaselessBatchConfig extends DefaultBatchConfiguration {
 
-    /**
-     * Creates a ResourcelessTransactionManager bean for handling transactions without persistent storage.
-     * This implementation is suitable for scenarios where transaction management isn't required,
-     * such as testing or simple batch processing workflows.
-     */
+    @Override
+    protected DataSource getDataSource() {
+        return new NoOpDataSource();
+    }
+
+    @Override
     @Bean
-    public PlatformTransactionManager transactionManager() {
+    protected PlatformTransactionManager getTransactionManager() {
         return new ResourcelessTransactionManager();
     }
 
-    /**
-     * Creates a ResourcelessJobRepository bean for managing job execution metadata without persistent storage.
-     * This implementation is suitable for scenarios where job execution data doesn't need to be persisted,
-     * such as testing or simple batch processing workflows.
-     *
-     * @see <a href="https://docs.spring.io/spring-batch/reference/whatsnew.html#new-resourceless-job-repository">Spring Batch Documentation</a>
-     */
-    @Bean
-    public JobRepository jobRepository() {
-        return new ResourcelessJobRepository();
-    }
 
-    /**
-     * Creates a JobLauncher bean responsible for executing batch jobs.
-     * The JobLauncher is a crucial component in Spring Batch that handles the execution
-     * of jobs, including their initialization and launch.
-     * We need to manually wire this when we exclude DataSourceAutoConfiguration.
-     */
-    @Bean
-    public JobLauncher jobLauncher(JobRepository jobRepository) {
-        TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
-        jobLauncher.setJobRepository(jobRepository);
-        return jobLauncher;
+    @Override
+    public JobRepository jobRepository() throws BatchConfigurationException {
+        return new ResourcelessJobRepository();
     }
 
     /**
@@ -78,5 +52,51 @@ public class DatabaselessBatchConfig {
     @Bean
     public ApplicationRunner batchRunner(Job job, JobLauncher jobLauncher) {
         return args -> jobLauncher.run(job, new JobParameters());
+    }
+
+    private static class NoOpDataSource implements DataSource {
+        @Override
+        public Connection getConnection() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public Connection getConnection(String username, String password) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public PrintWriter getLogWriter() throws SQLException {
+            return null;
+        }
+
+        @Override
+        public void setLogWriter(PrintWriter out) throws SQLException {
+        }
+
+        @Override
+        public void setLoginTimeout(int seconds) throws SQLException {
+
+        }
+
+        @Override
+        public int getLoginTimeout() throws SQLException {
+            return 0;
+        }
+
+        @Override
+        public <T> T unwrap(Class<T> iface) throws SQLException {
+            return null;
+        }
+
+        @Override
+        public boolean isWrapperFor(Class<?> iface) throws SQLException {
+            return false;
+        }
+
+        @Override
+        public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+            return null;
+        }
     }
 }
